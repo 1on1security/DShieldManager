fd629a8bb1b5719975c2ef73b061d9f9096f302c
Dustin Decker <dustin.decker@1on1security.com>
2024-01-31 23:03:03 +0000

    Massaging menu order and subs.

--- bin/simpleCursedDshield.bash
+++ bin/simpleCursedDshield.bash
@@ -16,32 +16,26 @@ main_menu() {
     --title "DShield Manager Main Menu" \
     --menu "Choose an option:" 15 50 7 \
     1 "Honeypot Log: Import/View by Date" \
-    2 "[-nothing-]" \
-    3 "Packets: Extract Tarballs" \
-    4 "Packets: to Security Onion" \
-    5 "Execute Remote Command on Sensors" \
-    6 "Status of DShield Sensors" \
-    7 "Graph File Sizes" \
-    8 "Import ALL Honeypot Logs (LONG)" \
+    2 "Packets: Extract Tarballs" \
+    3 "Packets: to Security Onion" \
+    4 "Graphing Menu" \
+    5 "Utilities Menu" \
     q "Exit" 2> /tmp/dshieldManager_choice
 
   choice=$(cat /tmp/dshieldManager_choice)
   case $choice in
     1) honeypot2SQL ; sqlitebrowser $dbDir/sql/webhoneypot.sqbpro & ;;
-    2) exit 0;;
-    3) extractPackets ;;
-    4) onionPackets ;;
-    5) remoteCommand ;;
-    6) sensorStatus ;;
-    7) graphFileSizes ;;
-    8) time allhoneypots2SQL ;;
+    2) extractPackets ;;
+    3) onionPackets ;;
+    4) graphFileSizes ;;
+    5) utilities ;;
     q) exit 0 ;;
   esac
 }
 
 graphFileSizes() {
-  dialog --clear --backtitle "Sub Menu" \
-    --title "Sub Menu" \
+  dialog --clear --backtitle "Graphing Menu" \
+    --title "Graphing Menu" \
     --menu "Choose an option:" 15 50 5 \
     1 "Detect Large Files (>150MB)" \
     2 "View Hourly Honeypot Log Sizes BY SENSOR" \
@@ -49,7 +43,7 @@ graphFileSizes() {
     4 "View Daily packet Tarball Sizes BY SENSOR" \
     5 "View Download File Sizes BY SENSOR" \
     6 "View TTY File Sizes BY SENSOR" \
-    x "Back to Main Menu" 2> /tmp/dshieldManager_choice
+    b "Back to Main Menu" 2> /tmp/dshieldManager_choice
 
   choice=$(cat /tmp/dshieldManager_choice)
   case $choice in
@@ -59,7 +53,29 @@ graphFileSizes() {
     4) graphPacketTarballs ;;
     5) graphDownloads ;;
     6) graphTTY ;;
-    x) main_menu ;;
+    b) main_menu ;;
+  esac
+}
+
+utilities() {
+  dialog --clear --backtitle "Utilities" \
+    --title "Utilities" \
+    --menu "Choose an option:" 15 50 5 \
+    1 "Sensors: Flush logs > 48 hours old." \
+    2 "Sensors: View Status of All Sensors" \
+    3 "Execute Remote Command on Sensors" \
+    4 "Import/Build the _HUGE_ Database" \
+    5 "Open the _HUGE_ Database" \
+    b "Back to Main Menu" 2> /tmp/dshieldManager_choice
+
+ choice=$(cat /tmp/dshieldManager_choice)
+ case $choice in
+    1) flushSensorLogs ;;
+    2) sensorStatus ;;
+    3) remoteCommand ;;
+    4) time allhoneypots2SQL ;;
+    5) sqlitebrowser $dbDir/sql/everywebhoneypot.db3.sqbpro & ;;
+    b) main_menu ;;
   esac
 }
 
